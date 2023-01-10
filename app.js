11c17d320ddb5cf831062ebd82eeb1ddafc01049
chairulakmal <akmal@macbook.local>
2023-01-10 10:01:47 +0700

    middlewares: body parser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const router = require("./routes");
 
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
 app.listen(3000, (_) => console.log("App is running at port", 3000));
