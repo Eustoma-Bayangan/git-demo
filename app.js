73293fcc4107a1131eb25b3059cf38987b9f08d8
chairulakmal <akmal@macbook.local>
2023-01-10 10:02:18 +0700

    feature: add body parser middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const router = require("./routes");
 
+app.use(require("express").urlencoded({ extended: false }));
 app.use(router);
 
 app.listen(3000, (_) => console.log("App is running at port", 3000));
